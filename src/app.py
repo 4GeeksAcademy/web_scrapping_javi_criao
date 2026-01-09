198fe5d52afef6a6adeb8a00c51f9eeb1c1a7d66
Javier Criao <javiercriao5@gmail.com>
2026-01-09 12:16:30 +0000

    mi primer scraping

--- src/app.py
+++ src/app.py
@@ -5,4 +5,10 @@ import time
 import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+
+url = "https://www.compraonline.alcampo.es/categories/bebidas/cervezas/cerveza-lata-est%C3%A1ndar/OC110701"
+response = requests.get(url)
+html_content = response.text
+print(f"HTML descargado. Estado: {response.status_code}")
 
